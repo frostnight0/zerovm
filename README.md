ca7e78e679e83d45cb905ddbacf1c5551cf28945
frostnight0 <westforest0@gmail.com>
2025-12-13 14:23:48 +0200

    up

--- README.md
+++ README.md
@@ -23,22 +23,96 @@ CPU:
         R0 (8bit)  - General Purpose
         R1 (8bit)  - General Purpose
         R2 (8bit)  - General Purpose
-        R3 (8bit)  - Flags
+        R3 (8bit)  - General Purpose
+        R4 (8bit)  - General Purpose
+        R5 (8bit)  - General Purpose
+        R6 (8bit)  - General Purpose
+        R7 (8bit)  - General Purpose
         PC (16bit) - Program Counter
+        Z  (bool)  - Zero Flag
+        N  (bool)  - Negative Flag
+        C  (bool)  - Unsigned Overflow / Carry Flag
+        V  (bool)  - Signed Overflow
 
     Instructions:
         00  NOP
-        10  MOV R0, imm8
-        11  MOV R1, imm8
-        12  MOV R2, imm8
-        13  MOV R3, imm8
+
+        10  MOV R0, [PC]        // MOV R0, imm8
+        11  MOV R1, [PC]        // MOV R1, imm8
+        12  MOV R2, [PC]        // MOV R2, imm8
+        13  MOV R3, [PC]        // MOV R3, imm8
+
+        20  MOV R0, [R3 R2]     // R0, [(R3 << 8) | R2]
+        21  MOV R1, [R3 R2]     // R1, [(R3 << 8) | R2]
+        22  MOV R2, [R1 R0]     // R2, [(R1 << 8) | R0]
+        23  MOV R3, [R1 R0]     // R3, [(R1 << 8) | R0]
+
+        30  MOV [R3 R2], R0     // [(R3 << 8) | R2], R0
+        31  MOV [R3 R2], R1     // [(R3 << 8) | R2], R1
+        32  MOV [R1 R0], R2     // [(R1 << 8) | R0], R2
+        33  MOV [R1 R0], R3     // [(R1 << 8) | R0], R3
+
+        40  ADD R0, R2          // Set: ZNCV
+        41  ADD R1, R3          // Set: ZNCV
+        42  ADC R0, R2          // Set: ZNCV
+        43  ADC R1, R3          // Set: ZNCV
+        44  NOT R0
+        45  NOT R1
+        46  AND R0, R2
+        47  AND R1, R3
+        48  XOR R0, R2
+        49  XOR R1, R3
+        4A  SHR R0
+        4B  SHR R1
+        4C  SHL R0
+        4D  SHL R1
+        4E  CMP R0, R2
+        4F  CMP R1, R3
+
+        61  MOV R0, R1
+        62  MOV R0, R2
+        63  MOV R0, R3
+        64  MOV R0, R4
+        65  MOV R0, R5
+        66  MOV R0, R6
+        67  MOV R0, R7
+        6A  MOV R1, R2
+        6B  MOV R1, R3
+        6C  MOV R1, R4
+        6D  MOV R1, R5
+        6E  MOV R1, R6
+        6F  MOV R1, R7
+
+        71  MOV R1, R0
+        72  MOV R2, R0
+        73  MOV R3, R0
+        74  MOV R4, R0
+        75  MOV R5, R0
+        76  MOV R6, R0
+        77  MOV R7, R0
+        7A  MOV R2, R1
+        7B  MOV R3, R1
+        7C  MOV R4, R1
+        7D  MOV R5, R1
+        7E  MOV R6, R1
+        7F  MOV R7, R1
+
         F0  HALT
 
+        Ideas:
+          1) PC as pair of GPRS
+          2) Rx0 hardwired to zero
+          3) Rx for flags
+          4) No flags
+          5) Stack based CPU
+
 RAM:
-    Backend: - redis
-    Address: - 16bit
-    Data:    - 8bit
+    Address: 16bit
+    Data:    8bit
 
 GPU:
     text mode: 80x25
+
+Features:
+    RAM backend: redis
 ```
--- src/bin/cpu.rs
+++ src/bin/cpu.rs
@@ -48,15 +48,26 @@ impl Ram {
 
 #[allow(non_snake_case)]
 struct CPU {
-    PC: u16,  // Program Counter
-    R0: u8,   // Accumulator / Addr
-    R1: u8,   // Data / Addr
-    R2: u8,   // Stack Pointer
-    R3: u8,   // Flags
+    PC: u16,    // Program Counter
+    R0: u8,     // General Purpose
+    R1: u8,     // General Purpose
+    R2: u8,     // General Purpose
+    R3: u8,     // General Purpose
+    R4: u8,     // General Purpose
+    R5: u8,     // General Purpose
+    R6: u8,     // General Purpose
+    R7: u8,     // General Purpose
+    // -- flags --
+    Z: bool,    // Zero
+    N: bool,    // Negative
+    C: bool,    // Carry / Unsigned Overflow
+    V: bool,    // Signed Overflow
 
     // -- internal vars --
     halted: bool,  // Halt Flag
     instr:  u8,    // Instruction
+    opcode: u8,    // Opcode (4bit)
+    arg:    u8,    // Argument (4bit)
     
     // -- debug settings --
     fetch_debug: bool,   // Enable instruction output
@@ -65,16 +76,25 @@ struct CPU {
     ram: Ram,
 }
 
+const ZF: u8 = 1 << 7;
+const NF: u8 = 1 << 6;
+const CF: u8 = 1 << 5;
+const VF: u8 = 1 << 4;
+
 impl CPU {
     fn new(ram: Ram) -> Self {
         Self {
             PC: 0x0000,
             R0: 0x00, R1: 0x00, R2: 0x00, R3: 0x00,
+            R4: 0x00, R5: 0x00, R6: 0x00, R7: 0x00,
+            Z: false, N: false, C: false, V: false,
 
             // -- internal vars --
             halted: false,
             instr:  0x00,
-            
+            opcode: 0x00,
+            arg:    0x00,
+
             // -- debug settings --
             fetch_debug: false,
 
@@ -83,40 +103,167 @@ impl CPU {
         }
     }
 
+    fn sr(&self) -> u8 {
+        let mut sr: u8 = 0;
+        if self.Z { sr = sr | ZF }
+        if self.N { sr = sr | NF }
+        if self.C { sr = sr | CF }
+        if self.V { sr = sr | VF }
+        sr
+    }
+
+    fn set_rs(&mut self, sr: u8) {
+        self.Z = (sr & ZF) != 0;
+        self.N = (sr & NF) != 0;
+        self.C = (sr & CF) != 0;
+        self.V = (sr & VF) != 0;
+    }
+
+    fn flags_str(&self) -> String {
+        format!(
+            "{}{}{}{}",
+            if self.Z { 'Z' } else { '-' },
+            if self.N { 'N' } else { '-' },
+            if self.C { 'C' } else { '-' },
+            if self.V { 'V' } else { '-' },
+        )
+    }
+
     fn print_debug(&self) {
-        println!("PC:[{:04X}], R0:[{:02X}], R1:[{:02X}], R2:[{:02X}], R3:[{:02X}]", self.PC, self.R0, self.R1, self.R2, self.R3);
+        println!("PC:[{:04X}], R0:[{:02X}], R1:[{:02X}], R2:[{:02X}], R3:[{:02X}], R4:[{:02X}], R5:[{:02X}], R6:[{:02X}], R7:[{:02X}], SR:[{}]",
+            self.PC, self.R0, self.R1, self.R2, self.R3, self.R4, self.R5, self.R6, self.R7, self.flags_str());
     }
 
     fn fetch(&mut self) {
         self.instr = self.ram.read(self.PC);
-        self.PC += 1;
+        self.incpc();
 
         if self.fetch_debug {
             println!("{}", format!("instr: {:02X}", self.instr).blue());
         }
     }
 
+    fn incpc(&mut self) {
+        self.PC += 1;
+    }
+
+    fn decpc(&mut self) {
+        self.PC -= 1;
+    }
+
+    fn alu_add(&mut self, a: u8, b: u8) -> u8 {
+        let tmp = a as u16 + b as u16;
+        let r = (tmp & 0xFF) as u8;
+
+        self.Z = r == 0;
+        self.N = (r & 0x80) != 0;
+        self.C = tmp > 0xFF;
+        self.V = (!(a ^ b) & (a ^ r) & 0x80) != 0;
+
+        r
+    }
+
+    fn alu_adc(&mut self, a: u8, b: u8) -> u8 {
+        let c :u16 = if self.C { 1 } else { 0 };
+        let tmp = a as u16 + b as u16 + c;
+        let r = (tmp & 0xFF) as u8;
+
+        self.Z = r == 0;
+        self.N = (r & 0x80) != 0;
+        self.C = tmp > 0xFF;
+        self.V = (!(a ^ b) & (a ^ r) & 0x80) != 0;
+
+        r
+    }
+
     fn execute(&mut self) {
-        let opcode = self.instr >> 4;
-        let arg = self.instr & 0x0F;  
+        self.opcode = self.instr >> 4;
+        self.arg = self.instr & 0x0F;  
 
-        match opcode {
-            0x0 => {},                              // NOP
+        match self.opcode {
+            0x0 => {},                                  // NOP
 
-            0x1 => {                                // MOV Rx, imm8
-                let temp = self.ram.read(self.PC);
-                self.PC += 1;
+            0x1 => {                                    // MOV Rx, [PC]
+                self.fetch();
 
-                match arg {
-                    0x0 => self.R0 = temp,          // MOV R0, imm8
-                    0x1 => self.R1 = temp,          // MOV R1, imm8
-                    0x2 => self.R2 = temp,          // MOV R2, imm8
-                    0x3 => self.R3 = temp,          // MOV R3, imm8
-                    _ => panic!("{}", format!("CPU: Unknown arg [{:X}]", arg).red().bold()),
+                match self.arg {
+                    0x0 => self.R0 = self.instr,        // MOV R0, [PC]
+                    0x1 => self.R1 = self.instr,        // MOV R1, [PC]
+                    _ => panic!("{}", format!("CPU: Unknown arg [{:X}]", self.arg).red().bold()),
                 }
             },
 
-            0xF => {
+            0x2 => {                                    // MOV Rx, [R1 R0]
+match self.arg {
+                    0x2 => self.R2 = self.ram.read((self.R1 as u16) << 4 | self.R0 as u16),     // MOV R2, [R1 R0]
+                    0x3 => self.R3 = self.ram.read((self.R1 as u16) << 4 | self.R0 as u16),     // MOV R3, [R1 R0]
+                    _ => panic!("{}", format!("CPU: Unknown arg [{:X}]", self.arg).red().bold()),
+                }
+            },
+
+            0x3 => {                                    // MOV [R1 R0], Rx
+                match self.arg {
+                    0x2 => self.ram.write((self.R1 as u16) << 4 | self.R0 as u16, self.R2),     // MOV [R1 R0], R2
+                    0x3 => self.ram.write((self.R1 as u16) << 4 | self.R0 as u16, self.R3),     // MOV [R1 R0], R3
+                    _ => panic!("{}", format!("CPU: Unknown arg [{:X}]", self.arg).red().bold()),
+                }
+            },
+
+            0x4 => {                                    // ALU R0, R1
+                match self.arg {
+                    0x0 => self.R0 = self.alu_add(self.R0, self.R2),
+                    0x1 => self.R1 = self.alu_add(self.R1, self.R3),
+                    0x3 => self.R0 = self.alu_adc(self.R0, self.R2),
+                    0x4 => self.R1 = self.alu_adc(self.R1, self.R3),
+                    _ => panic!("{}", format!("CPU: Unknown arg [{:X}]", self.arg).red().bold()),
+                }
+
+            },
+            0x5 => {},                                  // JMP [R1 R0]
+
+            0x6 => {                                    // MOV R0/R1, Rx
+                match self.arg {
+                    0x1 => self.R0 = self.R1,           // MOV R0, R1
+                    0x2 => self.R0 = self.R2,           // MOV R0, R2
+                    0x3 => self.R0 = self.R3,           // MOV R0, R3
+                    0x4 => self.R0 = self.R4,           // MOV R0, R4
+                    0x5 => self.R0 = self.R5,           // MOV R0, R5
+                    0x6 => self.R0 = self.R6,           // MOV R0, R6
+                    0x7 => self.R0 = self.R7,           // MOV R0, R7
+                    0x8 => self.R1 = self.R0,           // MOV R1, R0
+                    0xA => self.R1 = self.R2,           // MOV R1, R2
+                    0xB => self.R1 = self.R3,           // MOV R1, R3
+                    0xC => self.R1 = self.R4,           // MOV R1, R4
+                    0xD => self.R1 = self.R5,           // MOV R1, R5
+                    0xE => self.R1 = self.R6,           // MOV R1, R6
+                    0xF => self.R1 = self.R7,           // MOV R1, R7
+                    _ => panic!("{}", format!("CPU: Unknown arg [{:X}]", self.arg).red().bold()),
+                }
+            },
+            
+            0x7 => {                                    // MOV Rx, R0/R1
+                match self.arg {
+                    0x1 => self.R1 = self.R0,           // MOV R1, R0
+                    0x2 => self.R2 = self.R0,           // MOV R2, R0
+                    0x3 => self.R3 = self.R0,           // MOV R3, R0
+                    0x4 => self.R4 = self.R0,           // MOV R4, R0
+                    0x5 => self.R5 = self.R0,           // MOV R5, R0
+                    0x6 => self.R6 = self.R0,           // MOV R6, R0
+                    0x7 => self.R7 = self.R0,           // MOV R7, R0
+                    0x8 => self.R0 = self.R1,           // MOV R0, R1
+                    0xA => self.R2 = self.R1,           // MOV R2, R1
+                    0xB => self.R3 = self.R1,           // MOV R3, R1
+                    0xC => self.R4 = self.R1,           // MOV R4, R1
+                    0xD => self.R5 = self.R1,           // MOV R5, R1
+                    0xE => self.R6 = self.R1,           // MOV R6, R1
+                    0xF => self.R7 = self.R1,           // MOV R7, R1
+                    _ => panic!("{}", format!("CPU: Unknown arg [{:X}]", self.arg).red().bold()),
+                }
+            },
+
+            0x8 => {},                              // INC/DEC Rx
+
+            0xF => {                                // HALT
                 self.halted = true;
             },
 
@@ -127,11 +274,20 @@ impl CPU {
 
 fn preload_ram(ram: &mut Ram) {
     ram.write(0x0000, 0x00);   // NOP
-    ram.write(0x0001, 0x10);   // MOV R0, imm8
-    ram.write(0x0002, 0x11);   // 0x11
-    ram.write(0x0003, 0x11);   // MOV R1, imm8
-    ram.write(0x0004, 0x22);   // 0x22
-    ram.write(0x0005, 0xF0);   // HALT
+    ram.write(0x0001, 0x10);   // MOV R0, [PC]
+    ram.write(0x0002, 0x55);   // 0x55
+    ram.write(0x0003, 0x72);   // MOV R2, R0
+    ram.write(0x0004, 0x10);   // MOV R0, [PC]
+    ram.write(0x0005, 0x11);   // 0x11
+    ram.write(0x0006, 0x11);   // MOV R1, [PC]
+    ram.write(0x0007, 0x22);   // 0x22
+    ram.write(0x0008, 0x32);   // MOV [R1 R0], R2 | [2211] <= 0x55
+    ram.write(0x0009, 0x23);   // MOV R3, [R1 R0] | R3 <= [2211] 0x55
+    ram.write(0x000A, 0x40);   // ADD R0, R1
+    ram.write(0x000B, 0x43);   // ADD R0, R1
+    ram.write(0x000C, 0x43);   // ADD R0, R1
+    ram.write(0x000D, 0x43);   // ADD R0, R1
+    ram.write(0x000E, 0xF0);   // HALT
 }
 
 fn main() {
@@ -140,7 +296,7 @@ fn main() {
 
     let mut cpu = CPU::new(ram);
 
-    println!("-- CPU STARTED ---------------------------------------------------");
+    println!("-- CPU STARTED -----------------------------------------------------------------------------");
 
     cpu.print_debug();
     cpu.fetch_debug = true;
@@ -152,7 +308,7 @@ fn main() {
     }
 
     println!("{}", "CPU halted.".magenta());
-    println!("-- CPU ENDED -----------------------------------------------------");
+    println!("-- CPU ENDED -------------------------------------------------------------------------------");
     
 }
 
